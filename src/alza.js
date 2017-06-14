f6fce010387de347cc9e7a799ab0c5914c66c699
Samuel Hapák <samuel.hapak@gmail.com>
2017-06-14 13:06:54 +0200

    Adding to cart

--- src/alza.js
+++ src/alza.js
@@ -1,12 +1,19 @@
 import _request from 'request-promise'
 
+const jar = _request.jar()
 const request = _request.defaults({
   headers: {'Content-Type': 'application/json'},
-  jar: _request.jar(),
+  jar,
 })
 
 const SCV='https://www.alza.sk/Services/EShopService.svc/'
 
 export function* login(credentials) {
   return yield request.post(`${SCV}LoginUser`, {body: JSON.stringify(credentials)})
+}
+
+export function* addToCart(item, count) {
+  return yield request.post(`${SCV}OrderCommodity`, {body: JSON.stringify(
+    {id: item, count}
+  )})
 }
