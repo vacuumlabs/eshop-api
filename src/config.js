1a3ed0ab2b74ba8a4b2e32b3a358cd8d1892820b
Samuel Hapák <samuel.hapak@gmail.com>
2017-06-14 13:43:25 +0200

    Add order items function and test it in index.

--- src/config.js
+++ src/config.js
@@ -2,5 +2,11 @@ import transenv from 'transenv'
 export default transenv()(({str, bool}) => {
   return {
     port: str('PORT'),
+    alza: {
+      credentials: {
+        userName: str('alza-username'),
+        password: str('alza-password'),
+      }
+    }
   }
 })
--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import c from './config'
 import express from 'express'
 import {expressHelpers, run} from 'yacol'
+import {login, addToCart} from './alza'
 
 const app = express()
 const {register, runApp} = expressHelpers
 
+function* orderItems(items) {
+  yield run(login, c.alza.credentials)
+  for (let item of items) {
+    yield run(addToCart, item)
+  }
+}
+
 function* index(req, res) {
+  yield run(orderItems, [
+    {url: 'https://www.alza.sk/rowenta-rh8895wo-airforce-extreme-32-4v-d4945936.htm', count: 3},
+    {url: 'https://www.alza.sk/asus-gt1030-sl-2g-brk-d4943984.htm', count: 12},
+    {url: 'https://www.alza.sk/samsung-galaxy-a3-2016-sm-a310f?dq=3955169', count: 1},
+  ])
   res.send('Hello World')
 }
 
