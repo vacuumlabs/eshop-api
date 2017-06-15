665f98c5bf6ddf4a9d2d7800cee61cf0934cdb4f
Samuel Hapák <samuel.hapak@gmail.com>
2017-06-15 15:56:33 +0200

    Acknowledge Alza order on Slack.
    Responds with order summary with few dummy buttons.

--- src/slack.js
+++ src/slack.js
@@ -1,10 +1,18 @@
+import c from './config'
 import _request from 'request-promise'
 import {run, createChannel} from 'yacol'
+import {login, getInfo} from './alza'
 import WS from 'ws'
 
 const API = 'https://slack.com/api/'
 
 const request = _request.defaults({})
+
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'EUR',
+  minimumFractionDigits: 2,
+});
 
 let state = {}
 
@@ -54,7 +62,59 @@ function* listen(channel) {
 function* listenUser(channel) {
   while (true) {
     const event = yield channel.take()
-    yield run(apiCall, 'chat.postMessage', state.token,
-              {channel: event.user, text: 'Thank you!', as_user: true})
+    const order = yield run(orderInfo, parseOrder(event.text))
+
+    const result = yield run(apiCall, 'chat.postMessage', state.token, {
+        channel: event.user,
+        attachments: JSON.stringify([orderToAttachment(order)]),
+        as_user: true,
+    })
   }
+}
+
+function orderToAttachment(order) {
+  const fields = order.items.map((item) => {
+    const itemSum = formatter.format(item.count * item.price)
+    const itemPrice = formatter.format(item.price)
+    return {
+      title: `${item.count} x ${item.name}`,
+      value: `<${item.url}|${itemSum} (${item.count} x ${itemPrice})>`,
+      short: true,
+    }
+  })
+
+  return {
+    title: `Total value: ${formatter.format(order.totalPrice)}`,
+    pretext: `Please confirm your order:`,
+    fields,
+    actions: [
+      {name: 'personal', text: 'Make Personal Order', type: 'button', value: 'personal'},
+      {name: 'company', text: 'Make Company Order', type: 'button', value: 'company'},
+      {name: 'cancel', text: 'Cancel Order', type: 'button', value: 'cancel', style: 'danger'},
+    ],
+  }
+
+}
+
+function parseOrder(text) {
+  const re = /\s*(\d*)\s*<(http[^\s]+)>/g
+  let matches
+  let goods = []
+  while ((matches = re.exec(text)) !== null) {
+    if (matches[1] === '') matches[1] = '1'
+    goods.push({url: matches[2], count: parseInt(matches[1])})
+  }
+  return goods
+}
+
+function* orderInfo(items) {
+  let info = []
+  let totalPrice = 0
+  yield run(login, c.alza.credentials)
+  for (let item of items) {
+    const itemInfo = yield run(getInfo, item.url)
+    info.push({...itemInfo, count: item.count, url: item.url})
+    totalPrice += itemInfo.price * item.count
+  }
+  return {items: info, totalPrice}
 }
