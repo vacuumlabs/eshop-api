4f554c20232cf886f53a9da2941fe1a0be40768d
Samuel Hapák <samuel.hapak@gmail.com>
2017-06-17 15:09:44 +0200

    Create simple db schema for user orders.

---
+++ src/knex/migrations/20170617145706_create_tables.js
@@ -0,0 +1,24 @@
+exports.up = async (knex) => {
+
+  await knex.schema.createTableIfNotExists('order', (table) => {
+    table.increments('id').primary()
+    table.string('user').notNullable()
+    table.string('ts').notNullable()
+    table.boolean('isCompany').notNullable()
+    table.text('reason')
+  });
+
+  await knex.schema.createTableIfNotExists('orderItem', (table) => {
+    table.increments('id').primary()
+    table.integer('order').references('order.id')
+    table.string('shopId').notNullable()
+    table.integer('count').notNullable()
+    table.string('url').notNullable()
+    table.decimal('price')
+  });
+}
+
+exports.down = async (knex) => {
+  await knex.schema.dropTable('order')
+  await knex.schema.dropTable('orderItem')
+};
