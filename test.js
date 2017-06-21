ba749eaee2e0ed59735d8e2d134e603e5b943813
Samuel Hapák <samuel.hapak@gmail.com>
2017-06-21 10:33:08 +0200

    Improve name scraping from Alza.
    Scrapping twitter title leads to better, more descriptive names.
    
    Compare (old vs. new):
    "DJI Phantom 4 Advanced" vs. "Inteligentný dron DJI Phantom 4 Advanced"

--- test.js
+++ test.js
@@ -3,8 +3,19 @@ import {getInfo} from './src/alza'
 import {connect, listen} from './src/slack'
 import c from './src/config'
 
-//run(getInfo, 'https://www.alza.sk/dji-phantom-4-advanced-d4844052.htm')
+run(function*() {
+  const urls = [
+    'https://www.alza.sk/rowenta-rh8895wo-airforce-extreme-32-4v-d4945936.htm',
+    'https://www.alza.sk/dji-phantom-4-advanced-d4844052.htm',
+    'https://www.alza.sk/macbook-pro-15-2016?dq=4534009&o=1',
+  ]
 
-run(function* () {
-  yield run(connect, c.slack.botToken)
+  for (const url of urls) {
+    const info = yield run(getInfo, url)
+    console.log(info)
+  }
 })
+
+/*run(function* () {
+  yield run(connect, c.slack.botToken)
+})*/
