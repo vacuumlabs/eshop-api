1274490a3c663cc167f0dcd591016c37ee18e4b6
Samuel Hapák <samuel.hapak@gmail.com>
2017-06-21 08:39:32 +0200

    Fix airtable throttling race condition.
    Previous version had a race condition in the waitFor method. When two users
    simultaneously ran the waitFor method, they could have get incorrect answers.
    
    To fix this, introduce a single bottleneck, the function which has only on
    instance running and consuming data from a channel.

--- src/airtable.js
+++ src/airtable.js
@@ -1,6 +1,6 @@
 import airtable from 'airtable'
 import c from './config'
-import {run} from 'yacol'
+import {run, createChannel} from 'yacol'
 
 const base = new airtable({apiKey: c.airtable.apiKey}).base(c.airtable.base)
 
@@ -8,20 +8,30 @@ const times = []
 const countLimit = 5
 const timeLimit = 1000
 
+const pendingRequests = createChannel()
+
 function sleep(till) {
-  return new Promise((resolve) => setTimeout(resolve(value), till - Date.now()))
+  return new Promise((resolve) => setTimeout(resolve(), till - Date.now()))
 }
 
-function* waitForRequest() {
-  while (times.length >= countLimit) {
-    yield sleep(times[0] + timeLimit)
-    times.shift()
-  }
+run(function* throttleRequests() {
+  for (;;) {
+    while (times.length >= countLimit) {
+      yield sleep(times[0] + timeLimit)
+      times.shift()
+    }
 
-  times.push(Date.now())
+    (yield pendingRequests.take())()
+    times.push(Date.now())
+  }
+})
+
+
+function waitForRequest() {
+  return new Promise((resolve) => pendingRequests.put(resolve))
 }
 
 export function* create(table, record) {
-  yield run(waitForRequest)
+  yield waitForRequest()
   return yield base(table).create(record)
 }
