27e8e41aa7de2566616cbb967c0ec3f602d5fbc0
Samuel Hapák <samuel.hapak@gmail.com>
2018-02-15 11:39:08 +0100

    Add mobile test cases.

--- test.js
+++ test.js
@@ -4,13 +4,20 @@ import {connect, listen, apiCall} from './src/slack'
 import c from './src/config'
 
 (async function() {
-  const urls = [
-    'https://www.alza.sk/sony-hi-res-wh-1000xm2-bezova-d5116750.htm',
-    'https://www.alza.sk/rowenta-rh8895wo-airforce-extreme-32-4v-d4945936.htm',
-    'https://www.alza.sk/dji-phantom-4-advanced-d4844052.htm',
-    'https://www.alza.sk/macbook-pro-15-2016?dq=4534009&o=1',
-    'https://www.alza.sk/sandisk-ultra-dual-usb-drive-3-0-16gb-d4421444.htm',
+  const products = [
+    'sony-hi-res-wh-1000xm2-bezova-d5116750.htm',
+    'rowenta-rh8895wo-airforce-extreme-32-4v-d4945936.htm',
+    'dji-phantom-4-advanced-d4844052.htm',
+    'macbook-pro-15-2016?dq=4534009&o=1',
+    'sandisk-ultra-dual-usb-drive-3-0-16gb-d4421444.htm',
+    'apple-earpods-with-remote-and-mic-d4799110.htm?o=2',
   ]
+
+  const urls = [].concat(
+    ...products.map(
+      (u) => [`https://www.alza.sk/${u}`, `https://m.alza.sk/${u}`]
+    )
+  )
 
   for (const url of urls) {
     const info = await getInfo(url)
