a1db2303cb3456899e9d9f4db2b72a36f0948f53
rtrembecky <rtrembecky@gmail.com>
2021-10-12 16:00:14 +0200

    add wincent db tables

---
+++ src/knex/migrations/20211012135832_add_wincent_tables.js
@@ -0,0 +1,27 @@
+
+exports.up = async (knex) => {
+  await knex.schema.createTable('wincentOrder', (table) => {
+    table.increments('id').primary()
+    table.string('user').notNullable()
+    table.string('ts').notNullable()
+    table.boolean('isCompany').notNullable()
+    table.text('reason')
+    table.string('office').notNullable().defaultTo('')
+    table.boolean('isUrgent')
+    table.boolean('isHome')
+  })
+
+  await knex.schema.createTable('wincentOrderItem', (table) => {
+    table.increments('id').primary()
+    table.integer('order').references('wincentOrder.id')
+    table.string('shopId').notNullable()
+    table.integer('count').notNullable()
+    table.string('url', 1000).notNullable()
+    table.decimal('price')
+  })
+}
+
+exports.down = async (knex) => {
+  await knex.schema.dropTable('wincentOrderItem')
+  await knex.schema.dropTable('wincentOrder')
+}
