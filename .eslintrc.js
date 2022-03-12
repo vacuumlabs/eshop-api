a29c952bde3a2e275629ed3707ebcdd8ea53b955
rtrembecky <rtrembecky@gmail.com>
2022-03-12 22:17:02 +0100

    add prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: [
+    'vacuumlabs',
+
+    // Baseline configurations
+    'eslint:recommended',
+
+    // Disable ESLint rules conflicting with Prettier and use Prettier rules
+    'prettier',
+    'prettier/prettier',
+    'plugin:prettier/recommended',
+  ],
+
+  env: {
+    node: true,
+  },
+
+  // don't use extended react rules as this is not a react app
+  rules: {'react/no-deprecated': 'off'},
+}
